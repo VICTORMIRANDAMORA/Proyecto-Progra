89caa394d47b875daa134e2573d8fa1efe28bb37
Victor <victor.miranda.mora@est.una.ac.cr>
2024-09-02 23:20:20 -0600

    prueba de switch

--- ProyectoProgra/ProyectoProgra.cpp
+++ ProyectoProgra/ProyectoProgra.cpp
@@ -7,10 +7,23 @@ int main()
 {
 	int eleccion = 0;
 	printf("Hola! Bienvenido a Nueva Cinema! En que podemos ayudarte?\n");
-	printf("a. Para ver el archvo\nb. Para acceder a mantenimiento\nc. Para reservar\nd. Para comprar\n");
+	printf("1. Para ver el archvo\n2. Para acceder a mantenimiento\n3. Para reservar\n4. Para comprar\n");
 	scanf_s("%d", &eleccion);
-	printf("has escogido la opcion: ");
-	printf("%d", eleccion);
+	switch (eleccion) {
+	case 1:
+		printf("ahora deberias estar viendo el archivo\n");
+		break;
+	case 2:
+		printf("Debes tener accceso para este menu\n");
+		break;
+	case 3:
+		printf("Vas a reservar un espacio en la sala\n");
+		break;
+	case 4:
+		printf("vas a pagar el tiquete que reservaste\n");
+		break;
+	}
+
 
 
 	return 0;
